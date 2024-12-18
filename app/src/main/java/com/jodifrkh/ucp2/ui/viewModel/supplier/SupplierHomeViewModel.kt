b292aeb729b5f5537a45dc8a5a590af5125683a9
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-18 17:46:58 +0700

    making view model with data class where it contains list of supplier

--- app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/supplier/SupplierHomeViewModel.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/supplier/SupplierHomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.jodifrkh.ucp2.ui.viewModel.supplier
 
+import com.jodifrkh.ucp2.data.entity.Supplier
+
+data class HomeUIState (
+    val listSpl : List<Supplier> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
