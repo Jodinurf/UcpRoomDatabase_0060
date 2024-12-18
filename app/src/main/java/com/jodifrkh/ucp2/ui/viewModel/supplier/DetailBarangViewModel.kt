0bd9cc493ecbd98218f2b61c673da500b8a4e517
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-18 18:47:57 +0700

    membuat function untuk mendefinisikan data yang ditampilkan

--- app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/supplier/DetailBarangViewModel.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/supplier/DetailBarangViewModel.kt
@@ -1,2 +1,14 @@
 package com.jodifrkh.ucp2.ui.viewModel.supplier
 
+import com.jodifrkh.ucp2.data.entity.Barang
+
+fun Barang.toDetaiUIEvent() : BarangEvent {
+    return BarangEvent(
+        id = id,
+        namaBarang = namaBarang,
+        deskripsi = deskripsi,
+        harga = harga,
+        stok = stok,
+        namaSupplier = namaSupplier
+    )
+}
