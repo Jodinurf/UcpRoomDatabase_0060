2fd05122fc2b16b91eba51b8d04049a0774d7e0b
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-18 17:35:24 +0700

    membuat data class HomeUIState untuk view model homepage Barang

--- app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/barang/BarangHomeViewModel.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/viewModel/barang/BarangHomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.jodifrkh.ucp2.ui.viewModel.barang
 
+import com.jodifrkh.ucp2.data.entity.Barang
+
+data class HomeUIState (
+    val listBarang: List<Barang> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
