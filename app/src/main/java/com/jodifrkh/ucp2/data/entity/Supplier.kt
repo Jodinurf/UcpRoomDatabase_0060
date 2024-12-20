b484c2a99501c4faaa74f5ed32b4c1710926cb46
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 03:00:16 +0700

    updated primary key on Supplier with auto increment

--- app/src/main/java/com/jodifrkh/ucp2/data/entity/Supplier.kt
+++ app/src/main/java/com/jodifrkh/ucp2/data/entity/Supplier.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "tblSupplier")
 data class Supplier(
     @PrimaryKey (autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val nama: String,
     val kontak: String,
     val alamat: String
