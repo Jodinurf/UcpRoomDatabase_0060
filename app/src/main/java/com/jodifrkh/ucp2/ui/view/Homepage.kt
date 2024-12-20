c1ce994d9db0f40d5ab947e04ae00219e4fed3be
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 03:14:35 +0700

    membuat halaman awal

--- app/src/main/java/com/jodifrkh/ucp2/ui/view/Homepage.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/view/Homepage.kt
@@ -1,2 +1,108 @@
 package com.jodifrkh.ucp2.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jodifrkh.ucp2.R
+
+@Preview(showBackground = true)
+@Composable
+fun Homepage(
+    modifier: Modifier = Modifier,
+    onItemClick: (String) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 30.dp)
+            .background(color = Color.White)
+    ) {
+        HeaderSection()
+    }
+}
+
+@Composable
+fun HeaderSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomEnd = 48.dp))
+            .background(color = colorResource(R.color.primary))
+            .padding(bottom = 32.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, top = 24.dp)
+        ) {
+            Column {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = "Home Icon",
+                    tint = Color.White,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Spacer(Modifier.padding(3.dp))
+                Text(
+                    text = "Welcome",
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Text(
+                    text = "to Our Warehouse",
+                    fontSize = 22.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
+        Box(
+            Modifier.align(Alignment.CenterEnd)
+                .padding(24.dp)
+                .padding(top = 12.dp)
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.warehouse),
+                contentDescription = "Photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+        }
+    }
+}
+
+
+
+
