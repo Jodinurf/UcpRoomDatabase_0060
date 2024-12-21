7eb040863382dd11ac70d3fbdd30a6e9dff21cfd
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 19:03:32 +0700

    membuat component untuk template

--- app/src/main/java/com/jodifrkh/ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/view/barang/DetailBrgView.kt
@@ -1,2 +1,33 @@
 package com.jodifrkh.ucp2.ui.view.barang
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetailBrg(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: String,
+) {
+    Column (
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "$title :",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = content,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
