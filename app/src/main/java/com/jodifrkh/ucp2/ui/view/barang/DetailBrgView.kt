8acf907afe289dadb846c2c2020ce17452d87150
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 19:11:10 +0700

    membuat list dari data - data suatu detail barang

--- app/src/main/java/com/jodifrkh/ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/jodifrkh/ucp2/ui/view/barang/DetailBrgView.kt
@@ -1,13 +1,52 @@
 package com.jodifrkh.ucp2.ui.view.barang
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jodifrkh.ucp2.data.entity.Barang
+
+@Composable
+fun ItemDetailBrg(
+    modifier: Modifier = Modifier,
+    barang: Barang
+) {
+    Card (
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(12.dp)
+        ) {
+            ComponentDetailBrg(title = "ID", content = barang.id.toString())
+            Spacer(modifier = Modifier.padding(16.dp))
+            ComponentDetailBrg(title = "Nama Barang", content = barang.namaBarang)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(title = "Deskripsi", content = barang.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(title = "Harga", content = barang.harga.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(title = "Stok", content = barang.stok.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(title = "Nama Supplier", content = barang.namaSupplier)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+
+}
 
 @Composable
 fun ComponentDetailBrg(
