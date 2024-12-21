d538022e4e6f5cce46e76a886dd64acd0b34db6c
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 23:17:33 +0700

    updated object NamaSupplier

--- app/src/main/java/com/jodifrkh/ucp2/data/NamaSupplier.kt
+++ app/src/main/java/com/jodifrkh/ucp2/data/NamaSupplier.kt
@@ -4,13 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.jodifrkh.ucp2.ui.viewModel.barang.BarangViewModel
+import com.jodifrkh.ucp2.ui.viewModel.PenyediaViewModel
+import com.jodifrkh.ucp2.ui.viewModel.supplier.SupplierHomeViewModel
 
 object NamaSupplier {
     @Composable
-    fun options(): List<String> {
-        val supplierViewModel: BarangViewModel = viewModel()
-        val supplierNames by supplierViewModel.supplierNames.collectAsState()
-        return supplierNames
+    fun options(
+        supplierHomeViewModel: SupplierHomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    ): List<String> {
+        val dataNama by supplierHomeViewModel.homeUiStateSpl.collectAsState()
+        val namaSupplier = dataNama.listSpl.map { it.nama }
+        return namaSupplier
     }
 }
