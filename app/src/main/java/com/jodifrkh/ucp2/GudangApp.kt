ac2843b0c18c03ff0a15ad45dd883073a08ad0dc
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-21 12:36:02 +0700

    updated gudangApp as application

--- app/src/main/java/com/jodifrkh/ucp2/GudangApp.kt
+++ app/src/main/java/com/jodifrkh/ucp2/GudangApp.kt
@@ -1,6 +1,13 @@
 package com.jodifrkh.ucp2
 
 import android.app.Application
+import com.jodifrkh.ucp2.dependenciesinjection.ContainerApp
 
 class GudangApp: Application() {
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
 }
